146b3574f9c7745b5423dd19c207b5192f28616a
KimHyeshin <hyeshin2530@gmail.com>
2019-07-09 22:37:32 +0900

    csApi 연동 코드 작성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// lib
+import csApi from './lib/api/csApi'
+
+var App = App || {};
+App.api = App.api || {};
+App.api.csApi = csApi
+
+window.App = App
+window.appVersion = "v0.0.1"
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
